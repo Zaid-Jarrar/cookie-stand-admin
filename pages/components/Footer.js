a63031b3d89ae3e7fd1e712525b1289ec4b455d8
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-06-13 16:41:57 +0300

    version2

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,7 +1,10 @@
-function Footer() {
+function Footer({counter}) {
   return (
     <footer className="px-5 py-3 bg-[#342c2c] fixed bottom-0 left-0 right-0">
-      <p className="text-sm text-white"> All Copyrights Reserved For Zaid Jarrar &copy; {new Date().getFullYear()}</p>
+      <div className="text-sm text-white">
+      {counter} Locations Worldwide
+      </div>
+      <p className="text-sm text-white">All Copyrights Reserved For Zaid Jarrar &copy; {new Date().getFullYear()}</p>
     </footer>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,39 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import Head from "next/head";
+// import Head from "next/head";
+import Head from "./components/Head"
 import { useState } from "react";
+import ReportTable from "./components/ReportTable";
 
 export default function Home() {
-  const [report, setReport] = useState({});
-
+  const [report, setReport] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [hourly_sales, setHourlySales] = useState([])
   function formHandle(event) {
     event.preventDefault();
+
+    setCounter(counter + 1);
+
     const addedLocation = {
       location: event.target.location.value,
-      minCookie: event.target.minimum_customers_per_hour.value,
-      maxCookie: event.target.maximum_customers_per_hour.value,
-      avgCookie: event.target.average_cookies_per_sale.value,
+      minCustomers: event.target.minimum_customers_per_hour.value,
+      maxCustomers: event.target.maximum_customers_per_hour.value,
+      avgCustomers: event.target.average_cookies_per_sale.value,
     };
-
-    setReport(addedLocation);
+    setReport([ ...report, addedLocation ]);
+    // setReport(addedLocation);
   }
   return (
     <div className="bg-[#1a0d1e] h-screen">
-      <Head>
-        <title>Cookie Admin Stand</title>
-      </Head>
+      <Head />
       <Header />
       <Main formHandle={formHandle} />
-      <h1 className="text-center text-white">Report table coming soon......</h1>
-      <h1 className="text-center text-white">{JSON.stringify(report)}</h1>
+      {/* <h1 className="text-center text-white">Report table coming soon......</h1>
+      <h1 className="text-center text-white">{JSON.stringify(report)}</h1> */}
+      { counter > 0 ? <ReportTable report={report} /> : <h2  className="text-center text-white">No Cookie Stands Available</h2>}
 
-      <Footer />
+      <Footer counter={counter}/>
     </div>
   );
 }
