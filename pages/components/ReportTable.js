f8efbd2a38db96796797cf7730e4cba4298eeac2
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-06-15 23:23:42 +0300

    login

--- pages/components/ReportTable.js
+++ pages/components/ReportTable.js
@@ -29,7 +29,10 @@ function ReportTable(props) {
 
                 return (
                     <tr className="py-2">
-                        <td className="border border-neutral-50 text-white">{location.location}</td>
+
+                        <td className="border border-neutral-50 text-white">{location.location}<svg xmlns="http://www.w3.org/2000/svg" className=" flex flex-row h-5 w-5 left-1" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                              </svg></td>
                         {hourly_sales.map(sales => (
                             <td className="border border-neutral-50 text-white">{sales}</td>))}
                             <td className="border border-neutral-50 text-white"> {hourly_sales.reduce((total,sales) => total += sales,0)}</td>
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,35 @@ import Footer from "./components/Footer";
 import Head from "./components/Head"
 import { useState } from "react";
 import ReportTable from "./components/ReportTable";
+import LoginForm from "./components/LoginForm";
+
+
+
+
 
 export default function Home() {
+
+
+  const [LoggedIn,setLogin] = useState(false)
+
   const [report, setReport] = useState([]);
   const [counter, setCounter] = useState(0);
   const [hourly_sales, setHourlySales] = useState([])
+  
+  function loginHandle(event) {
+    event.preventDefault();
+    const loginConfig = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+    function validateForm() {
+      if (loginConfig.username.length > 0 && loginConfig.password.length > 0) {
+        return true;}
+        else{
+          return false
+        }}   
+    {validateForm()?setLogin(true):alert("Please fill in all fields")}}
+
   function formHandle(event) {
     event.preventDefault();
 
@@ -25,15 +49,23 @@ export default function Home() {
     // setReport(addedLocation);
   }
   return (
-    <div className="bg-[#1a0d1e] h-screen">
-      <Head />
+    
+    <>
+      { LoggedIn ?
+        <div className="bg-[#1a0d1e] h-screen"> 
+        <Head />
       <Header />
+
+      
       <Main formHandle={formHandle} />
       {/* <h1 className="text-center text-white">Report table coming soon......</h1>
       <h1 className="text-center text-white">{JSON.stringify(report)}</h1> */}
       { counter > 0 ? <ReportTable report={report} /> : <h2  className="text-center text-white">No Cookie Stands Available</h2>}
 
-      <Footer counter={counter}/>
-    </div>
+      <Footer counter={counter}/> </div>: <LoginForm loginHandle={loginHandle}/>}
+  
+      </>
+      
+    
   );
 }
